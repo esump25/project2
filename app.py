45eda2412100ea504802096b7a0819053415295d
esump25 <emsumpter06@gmail.com>
2026-02-25 13:50:11 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def get_itinerary():
 
 if __name__ == '__main__':
     # Use 0.0.0.0 for Render deployment
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
